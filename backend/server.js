7234471531914ec89b907dd2b822174c5f136d49
Avinash Sahni <avinash.sahni@dgliger.com>
2026-05-26 12:17:54 +0530

    userfile name change

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.use("/api/cart", cartRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on Port ${PORT}`);
 });
